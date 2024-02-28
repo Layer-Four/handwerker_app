7a3d49bbe981295324c39dddc63270efa414f8b6
MmeissnerLayer4 <153509730+MmeissnerLayer4@users.noreply.github.com>
2024-02-28 13:05:17 +0100

    solved merge conflicts

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:handwerker_app/view/login_screen/login_view.dart';
-import 'package:handwerker_app/view/navigation_view/nav_layer.dart';
-import 'package:flutter/material.dart';
-import 'package:handwerker_app/view/login_screen/login_view.dart';
-// import 'package:handwerker_app/view/execution_view/execution_view.dart';
-import 'package:handwerker_app/view/login_screen/start.dart';
 import 'package:handwerker_app/view/navigation_view/nav_layer.dart';
 
 class AppRoutes {
