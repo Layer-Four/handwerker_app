dd6a8616f8fc4e8f99023dec76e4347003a9f3b3
obaidajarrah <153510718+obaidajarrah@users.noreply.github.com>
2024-03-22 09:44:42 +0100

    kj

--- lib/provider/user_provider/user_provider.dart
+++ lib/provider/user_provider/user_provider.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:handwerker_app/models/user.dart/user.dart';
 import 'package:http/http.dart' as http;
@@ -10,16 +8,8 @@ class UserNotifer extends Notifier<User> {
   @override
   User build() => const User();
   void addProject(User user) => state = user;
-  void userLogIn(String password, String email) async {
+  void userLogout() {
     final uri = Uri.parse('uri');
-
-    final response = await http.post(uri, body: state.userToken);
-    log(response.statusCode.toString());
-  }
-
-  void userLogout() async {
-    final uri = Uri.parse('uri');
-    final response = await http.post(uri, body: state.userToken);
-    log(response.statusCode.toString());
+    final response = http.post(uri, body: state.userToken);
   }
 }
