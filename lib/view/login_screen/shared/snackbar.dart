99155f91e52905e0580fef29f5d4201790137bbf
obaidajarrah <153510718+obaidajarrah@users.noreply.github.com>
2024-07-04 11:09:17 +0200

    snackbar anpassen

--- lib/view/login_screen/shared/snackbar.dart
+++ lib/view/login_screen/shared/snackbar.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-showSnackBar(BuildContext context, String text) {
-  return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    duration: const Duration(days: 1),
-    content: Center(
-        child: Text(
-      text,
-      textAlign: TextAlign.center,
-    )),
-    action: SnackBarAction(label: "Schließen", onPressed: () {}),
-  ));
+// showSnackBar(BuildContext context, String text) {
+//   return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+//     duration: const Duration(days: 1),
+//     content: Center(
+//         child: Text(
+//       text,
+//       textAlign: TextAlign.center,
+//     )),
+//     action: SnackBarAction(label: "Schließen", onPressed: () {}),
+//   ));
+// }
+
+showSnackBar(BuildContext context, String message) {
+  final snackBar = SnackBar(
+    content: Center(child: Text(message, textAlign: TextAlign.center)),
+  );
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
 }
